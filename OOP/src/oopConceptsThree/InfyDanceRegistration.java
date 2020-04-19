b7c0849282ffa5a9e24ed29f1dfddac9d3107eef
shalininarayan98 <sn.shalini1@hotmail.co.uk>
2020-04-19 16:19:14 +0100

    gitignore

---
+++ OOP/src/oopConceptsThree/InfyDanceRegistration.java
@@ -0,0 +1,21 @@
+package oopConceptsThree;
+
+public class InfyDanceRegistration {
+	
+	private int counter;
+	private String name;
+	private long contactNumber;
+	private String city;
+	
+	public InfyDanceRegistration(String name, long contactNumber, String city) {
+		super();
+		this.name = name;
+		this.contactNumber = contactNumber;
+		this.city = city;
+	}
+	
+	
+	
+	
+
+}
---
+++ OOP/src/oopConceptsThree/Tester.java
@@ -0,0 +1,5 @@
+package oopConceptsThree;
+
+public class Tester {
+
+}
